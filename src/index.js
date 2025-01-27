d923bf8969c36667d6d2268653fb2cc747ab6e4e
Iva <iva_dkostadinova@abv.bg>
2025-01-27 19:30:01 +0200

    create 404 page

--- src/index.js
+++ src/index.js
@@ -20,7 +20,13 @@ app.get('/about', (req, res)=>{
   res.render('about')
 });
 
+
+
 app.use('/static', express.static('src/public'));
+
+app.get('*',(req, res)=>{
+  res.render('404')
+})
 
 app.listen(5000, () =>
   console.log("Server is listening on http://localhost:5000...")
