f0f0bcc4a883c5ad7b054c1d7abdb1c469e99918
Iva <iva_dkostadinova@abv.bg>
2025-02-03 17:19:07 +0200

    add detailsPage with hardcoded data

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,5 +6,10 @@ movieController.get('/create', (req, res)=>{
     res.render('create')
 });
 
+movieController.get('/:movieId/details', (req, res)=>{
+    res.render('details')
+});
+
+
 export default movieController;
  
