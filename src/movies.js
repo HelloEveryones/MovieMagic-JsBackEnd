6625bc9b7d6942c8b517c18231a5e7c53d765a79
Iva <iva_dkostadinova@abv.bg>
2025-02-05 17:37:06 +0200

    add create movie and add uuid

--- src/movies.js
+++ src/movies.js
@@ -8,7 +8,7 @@ export default[
         imageUrl: 'https://i.ytimg.com/vi/Mn-ka4JMFdY/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGDggUihyMA8=&rs=AOn4CLAQMgOkn-QxTrtzc5U-EzTew6HSMw',
         director:'Iva Kostadinova',
         year:'2025',
-        rating:7
+        rating:'7'
     },
     {
         id:'2',
@@ -19,7 +19,7 @@ export default[
         imageUrl: 'https://cdn.ozone.bg/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/_/cbc8668ba87628a0e055e905bce700c5/ledena-epoha-5--golemiyat-sblasak-dvd-31.jpg',
         director:'Iva Dimitrova',
         year:'2024',
-        rating:5
+        rating:'5'
     },
     
 
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,3 +1,4 @@
+import {v4 as uuid} from 'uuid'
 import movies from '../movies.js'
 
  export default {
@@ -5,5 +6,16 @@ findOne(movieId){
 const result=movies.find(movie=>movie.id==movieId);
 
 return result; 
+},
+create(movieData){
+    const newId = uuid();
+movies.push({
+    id:newId,
+    ...movieData,
+});
+return newId;
+},
+getAll(){
+  return movies;  
 }
  }
